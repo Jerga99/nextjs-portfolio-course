6e63d48699f6536003f18559c93c34fef3b13983
Filip Jerga <filipjerga@Filips-MBP-2.fritz.box>
2020-04-01 16:58:14 +0200

    11 Navbar with React-Bootstrap

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,41 +1,38 @@
 
-const Navbar = () => {
+
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+
+const AppNavbar = () => {
 
   return (
     <div className="navbar-wrapper">
-      <nav className="navbar navbar-expand-lg navbar-dark fj-mw9">
-        <a className="navbar-brand mr-3 font-weight-bold" href="#">FilipJerga</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav">
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Portfolio</a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Courses</a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Cv</a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Ask me</a>
-            </li>
-          </ul>
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">Sign Up</a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link btn btn-success bg-green-2 bright" href="#">Sign In</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar expand="lg" className="navbar-dark fj-mw9">
+        <Navbar.Brand className="mr-3 font-weight-bold" href="#">FilipJerga</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="mr-auto">
+            <Nav.Link href="#" className="mr-3">
+              Portfolios
+            </Nav.Link>
+            <Nav.Link href="#" className="mr-3">
+              Forum
+            </Nav.Link>
+            <Nav.Link href="#" className="mr-3">
+              Cv
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#" className="mr-3">
+              Sign Up
+            </Nav.Link>
+            <Nav.Link href="#" className="mr-3 btn btn-success bg-green-2 bright">
+              Sign In
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   )
 }
 
-export default Navbar;
+export default AppNavbar;
