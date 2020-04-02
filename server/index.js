e9ed463546c23da0ef2c0ccf6789897e958ea884
Filip Jerga <filipjerga@Filips-MBP-2.fritz.box>
2020-04-02 11:07:16 +0200

    13-14 Implementing Graphql Starting

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const next = require('next')
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -8,6 +10,26 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
+
+  // Construct a schema, using GRAPHQL schema language
+  const schema = buildSchema(`
+      type Query {
+        hello: String
+      }
+  `);
+
+  // The root provides a resolver for each API endpoint
+  const root = {
+    hello: () => {
+      return 'Hello World!'
+    }
+  }
+
+  server.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  }));
 
   server.all('*', (req, res) => {
     return handle(req, res)
