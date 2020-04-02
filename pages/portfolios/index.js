4f9b0d2bd3cb159543e169248dd6a578b3a73411
Filip Jerga <filipjerga@Filips-MBP-2.fritz.box>
2020-04-02 13:51:15 +0200

    17 Fetch portfolios on client

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,19 +1,28 @@
 
+import axios from 'axios'
 
-
-const apiCall = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res({testingData: 'Just some testing Data'});
-    }, 200);
-  })
+const fetchPortfolios = () => {
+  const query = `
+    query Portfolios {
+      portfolios {
+        _id,
+        title,
+        company,
+        companyWebsite
+        location
+        jobTitle
+        description
+      }
+    }`;
+  return axios.post('http://localhost:3000/graphql', { query })
+    .then(({data: graph}) => graph.data)
+    .then(data => data.portfolios)
 }
 
+const Portfolios = ({portfolios}) => {
 
-const Portfolios = (props) => {
   return (
     <>
-      {props.testingData}
       <section className="section-title">
         <div className="px-2">
           <div className="pt-5 pb-4">
@@ -21,6 +30,7 @@ const Portfolios = (props) => {
           </div>
         </div>
       </section>
+      { JSON.stringify(portfolios) }
       <section className="pb-5">
         <div className="row">
           <div className="col-md-4">
@@ -66,9 +76,8 @@ const Portfolios = (props) => {
 }
 
 Portfolios.getInitialProps = async () => {
-  console.log('GET INITIAL PROPS PORTFOLIOS')
-  const data = await apiCall();
-  return { ...data };
+  const portfolios = await fetchPortfolios();
+  return { portfolios };
 }
 
 
