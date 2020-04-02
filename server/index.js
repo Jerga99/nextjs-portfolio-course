a3b7c300c65790946aa140af03ed7c4de2cb4031
Filip Jerga <filipjerga@Filips-MBP-2.fritz.box>
2020-04-02 11:29:57 +0200

    15 Graphql portfolios examples

--- server/index.js
+++ server/index.js
@@ -8,13 +8,52 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+
+const data = {
+  portfolios: [
+    {
+      _id: '32dsa8d9',
+      title: 'Job in USA',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false
+    },
+    {
+      _id: 'fa6dsa78',
+      title: 'Job in Barcelona',
+      content: 'It was very suny experience',
+      jobTitle: 'Developer',
+      isCurrentlyEmployed: true
+    },
+    {
+      _id: 'asd7a8d789',
+      title: 'Job in Germany',
+      content: 'It was very good!',
+      jobTitle: 'Manager',
+      daysOfExperience: 30,
+      isCurrentlyEmployed: true
+    },
+  ]
+}
+
 app.prepare().then(() => {
   const server = express()
 
   // Construct a schema, using GRAPHQL schema language
   const schema = buildSchema(`
+      type Porfolio {
+        _id: ID!
+        title: String
+        content: String
+        jobTitle: String,
+        daysOfExperience: Int,
+        isCurrentlyEmployed: Boolean
+      }
+
       type Query {
         hello: String
+        portfolio: Porfolio
+        portfolios: [Porfolio]
       }
   `);
 
@@ -22,6 +61,12 @@ app.prepare().then(() => {
   const root = {
     hello: () => {
       return 'Hello World!'
+    },
+    portfolio: () => {
+      return data.portfolios[0]
+    },
+    portfolios: () => {
+      return data.portfolios
     }
   }
 
