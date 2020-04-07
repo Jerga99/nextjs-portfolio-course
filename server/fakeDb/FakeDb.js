331d00b7c7e8b9f24080c59d8e5fd44296d11625
Filip Jerga <filipjerga@Filips-MBP-2.fritz.box>
2020-04-07 14:59:40 +0200

    43 Populate DB

--- server/fakeDb/FakeDb.js
+++ server/fakeDb/FakeDb.js
@@ -1,15 +1,20 @@
 
-
-
+const { portfolios } = require('./data');
+const Portfolio = require('../database/models/portfolio');
 
 class FakeDb {
 
-  populate() {
-
+  async clean() {
+    await Portfolio.deleteMany({});
   }
 
-  clean() {
+  async addData() {
+    await Portfolio.create(portfolios);
+  }
 
+  async populate() {
+    await this.clean();
+    await this.addData();
   }
 }
 
--- server/fakeDb/populate.js
+++ server/fakeDb/populate.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const config = require('../config/dev');
 const fakeDb = require('./FakeDb');
 
-
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}, () => {
-  console.log('Populating DB........');
-  console.log('Connected to DB');
+}, async () => {
+  console.log('Starting populating DB...');
+  await fakeDb.populate();
+  await mongoose.connection.close();
+  console.log('DB has been populated...');
 })
 
